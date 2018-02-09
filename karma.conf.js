8b94aae7fa6e726d392bac42ec530be926dbfa5a
Vitor Baptista <vitor@vitorbaptista.com>
2018-02-09 16:54:15 +0000

    Fix running tests on Karma
    I think there was some underlying package update that broke our tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,83 @@
-const webpackConfig = require('./webpack.config.js')
-delete webpackConfig.entry
-delete webpackConfig.resolve
-webpackConfig.externals = {
-  'cheerio': 'window',
-  'react/addons': true,
-  'react/lib/ExecutionEnvironment': true,
-  'react/lib/ReactContext': true
-}
+// Karma configuration
+// Generated on Fri Feb 09 2018 16:20:11 GMT+0000 (GMT)
 
-// Base
-
-const karmaConfig = (config) => {
+module.exports = function(config) {
   config.set({
-    singleRun: true,
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai'],
-    files: ['test/karma.opts'],
-    reporters: ['spec'],
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['mocha', 'sinon-chai'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/karma.opts',
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/karma.opts': ['webpack'],
     },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      noInfo: true
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [{
+          test: /\.(js|opts)$/,
+          exclude: /\/node_modules\//,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react'],
+          },
+        }],
+      },
     },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['spec'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
   })
 }
-
-// Module API
-
-module.exports = karmaConfig
